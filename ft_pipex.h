f686a02de7e7ee5409a2385eeda315eacd129274
tohsumi <tohsumi@student.42tokyo.jp>
2022-02-25 10:11:05 +0900

    arrange function prototype according to each file order

--- ft_pipex.h
+++ ft_pipex.h
@@ -23,23 +23,23 @@ enum
 };
 
 /* ft_utils.c */
-void	error(const char *funcname);
 void	free_2d_array(char **str);
+void	error(const char *funcname);
 
 /* ft_search_exe.c */
 void	execute(char *argv, char **envp);
 
 /* ft_utils_libft.c */
-void	ft_putstr_fd(const char *str, int fd);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strjoin(char const *s1, char const *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	ft_putstr_fd(const char *str, int fd);
 char	*ft_strdup(const char *s1);
 
 /* ft_pipex.c */
-void	pipex(char **argv, char **envp);
+void	parent_process(int pipefd[2], pid_t child2);
 void	launch_cmd1(int pipefd[2], char **argv, char **envp);
 void	launch_cmd2(int pipefd[2], char **argv, char **envp);
-void	parent_process(int pipefd[2], pid_t child2);
+void	pipex(char **argv, char **envp);
 
 /* ft_split.c */
 char	**ft_split(char const *s, char c);
