388c940fd367ea01c605cce52066c12928dc6cc9
tohsumi <tohsumi@student.42tokyo.jp>
2022-03-01 11:51:26 +0900

    [wip] norm
    rename filename which are used in bonus and modify makefile

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ SRCS = ft_pipex.c ft_split.c ft_utils.c ft_utils_libft.c ft_search_exe.c
 OBJS = $(SRCS:.c=.o)
 
 B_SRCS = ft_pipex_bonus.c ft_split_bonus.c ft_utils_bonus.c ft_utils_libft_bonus.c ft_search_exe_bonus.c \
-		 get_next_line.c get_next_line_utils.c
+		 get_next_line_bonus.c get_next_line_utils_bonus.c
 B_OBJS = $(B_SRCS:.c=.o)
 
 all: $(NAME)
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tohsumi <tohsumi@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/04 15:46:36 by tohsumi           #+#    #+#             */
+/*   Updated: 2022/03/01 11:49:37 by tohsumi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+int	ft_new_line_pos(const char *s, int chr)
+{
+	char	c;
+	int		i;
+
+	c = (char)chr;
+	i = 0;
+	if (!s)
+		return (-1);
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (i);
+		i++;
+	}
+	if (c == '\0')
+		return (i);
+	return (-1);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strndup(const char *s1, size_t n)
+{
+	char	*ptr;
+	int		i;
+
+	ptr = (char *)malloc(sizeof(char) * (n + 1));
+	if (!ptr)
+		return (NULL);
+	if (ptr == s1)
+		return (ptr);
+	i = 0;
+	while (i < (int)n)
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*ptr;
+	int		i;
+	int		j;
+
+	i = -1;
+	j = -1;
+	if (!s1 || !s2)
+		return (NULL);
+	ptr = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!ptr)
+		return (NULL);
+	while (s1[++i])
+		ptr[i] = s1[i];
+	while (s2[++j])
+		ptr[i + j] = s2[j];
+	ptr[i + j] = '\0';
+	return (ptr);
+}
+
+int	my_free(char **ptr1, char **ptr2, char **ptr3, char **ptr4)
+{
+	if (ptr1)
+	{
+		free(*ptr1);
+		*ptr1 = NULL;
+	}
+	if (ptr2)
+	{
+		free(*ptr2);
+		*ptr2 = NULL;
+	}
+	if (ptr3)
+	{
+		free(*ptr3);
+		*ptr3 = NULL;
+	}
+	if (ptr4)
+	{
+		free(*ptr4);
+		*ptr4 = NULL;
+	}
+	return (-1);
+}
